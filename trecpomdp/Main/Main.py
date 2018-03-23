2a01fa3a3d3e34c9da6caa6bd6ba94ba5c1185d0
DarkNightRequiem <13606030307@139.com>
2018-03-23 22:06:58 +0800

    2018.3.23 22:07

--- trecpomdp/Main/Main.py
+++ trecpomdp/Main/Main.py
@@ -1,12 +1,13 @@
-from ProcessingUtil.TrecDomParser import TrecDomParser
+from ProcessingUtil.TrecDomParser import TrecDomParser as tdp
 
 if __name__ == '__main__':
     # 在session track2012中有一个subject是"merck & co"在6140行左右，&是特殊字符解析或报错，我先简单替换成空格，分析无影响
-    tdp2012=TrecDomParser("D:\\SAL\\POMDP\\TRECData\\sessiontrack2012.xml")
-    tdp2013=TrecDomParser("D:\\SAL\\POMDP\\TRECData\\sessiontrack2013.xml")
-    sessions12=tdp2012.getElementByTagName("session")
-    sessions13=tdp2013.getElementByTagName("session")
+    sessionTrack12=tdp("D:\\SAL\\POMDP\\TRECData\\sessiontrack2012.xml")
+    sessionTrack13=tdp("D:\\SAL\\POMDP\\TRECData\\sessiontrack2013.xml")
 
-    # 根据时间对session进行排序
+    # 按照topic划分
+    sessionTrack12.divideAccordingTopic()
+    sessionTrack13.divideAccordingTopic()
+
 
     print("sssss")
--- trecpomdp/ProcessingUtil/TrecDomParser.py
+++ trecpomdp/ProcessingUtil/TrecDomParser.py
@@ -1,7 +1,11 @@
 from xml.dom.minidom import parse
 import xml.dom.minidom as minidom
+import datetime as dt
 
 
+# () 元组
+# [] 列表
+# {} 字典
 # 使用dom解析xml文件
 
 class TrecDomParser:
@@ -9,34 +13,50 @@ class TrecDomParser:
     def __init__(self, path):
         # 要解析的文件路径
         self.path = path
+        # 文件中的DomTree
         self.domTree = minidom.parse(path)
+        # 文件中的所有的dom元素
         self.collection = self.domTree.documentElement
-        self.tagNameList = ['sessiontrack2012', 'sessiontrack2013',
-                        'session',
-                            'topic',
-                                'subject',
-                                'desc',
-                                'narr',
-                            'interaction',
-                                'query',
-                                'results',
-                                    'result',
-                                        'url',
-                                        'clueweb09id','clueweb12id',
-                                        'title',
-                                        'snippet'
-                                'clicked',
-                                    'click',
-                                        'rank',
-                            'currentquery',
-                                'query']
+        # 所有的<session>
+        self.sessions = self.collection.getElementsByTagName("session")
 
-    def getElementByTagName(self, tagName):
+        self.tagNameList = ['sessiontrack2012', 'sessiontrack2013',
+                            'session',
+                            'topic',
+                            'subject',
+                            'desc',
+                            'narr',
+                            'interaction',
+                            'query',
+                            'results',
+                            'result',
+                            'url',
+                            'clueweb09id', 'clueweb12id',
+                            'title',
+                            'snippet'
+                            'clicked',
+                            'click',
+                            'rank',
+                            'currentquery',
+                            'query']
+
+    def divideAccordingTopic(self):
         """
-        根据tagName获取元素
+        将某年的sessiontrack数据按照topic划分
+        :return: 二维字典,第一维key是topic编号，第二维key是starttime
         """
-        if tagName in self.tagNameList:
-            elements = self.collection.getElementsByTagName(tagName)
-            return elements
-        else:
-            return None
+        sortedDict = {}
+        for session in self.sessions:
+            topic = session.getElementsByTagName("topic")
+            st=session.getAttribute("starttime")[0:-7]
+            starttime = dt.datetime.strptime(st, "%H:%M:%S")
+            topicId = topic[0].getAttribute("num")
+            if topicId not in sortedDict.keys():
+                sortedDict[topicId] = {}
+            if starttime not in sortedDict[topicId]:
+                sortedDict[topicId][starttime] = session
+        # TODO：对字典中的session按照starttime排序
+        for i in range(sortedDict.__len__()):
+            s=sortedDict[str(i+1)]
+        print("grouped sessions according to topics")
+        return sortedDict
